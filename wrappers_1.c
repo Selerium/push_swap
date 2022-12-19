dc58978960ca84f6f38ce4ad11fd038b1bdd342f
John Adithya <jadithya@lab1r2s12.42abudhabi.ae>
2022-12-19 21:29:47 +0400

    updated

--- wrappers_1.c
+++ wrappers_1.c
@@ -53,12 +53,19 @@ void	sort3_2(t_stack *a)
 
 void	generalsort(t_stack *a, t_stack *b, int n)
 {
-	int	median;
-
-	if (!b->items || n)
-		median = 0;
-	median = ft_median(a);
-	ft_printf("mid: %d\n", median);
-	ft_quicksort1(a, b, median);
-	ft_quicksort2(b, a, median);
+	if (n)
+		push(a, b, 1);
+	while (a->items > 0)
+	{
+		push(a, b, 1);
+		while (b->number[0] > b->number[1])
+		{
+			swap(b, 'b', 1);
+			if (b->items > 2)
+				rotate(b, 'b', 1);
+			test_stacks(a, b);
+		}
+		while (b->number[0] > b->number[b->items - 1])
+			rotate(b, 'b', 1);
+	}
 }
